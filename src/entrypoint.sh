a27dc4a092de0d6b86387809564904bfcf3445c7
Guillaume Simonneau <milu@localhost.localdomain>
2016-11-18 16:17:51 +0100

    fixing HEAP_SIZE managment

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,14 +2,15 @@
 
 set -m
 
-export LS_JAVA_OPTS="-Xms$HEAP_SIZE -Xmx$HEAP_SIZE"
 /run/miscellaneous/restore_config.sh
 /run/miscellaneous/wait_for_elasticsearch.sh
 
 # Run as user "logstash" if the command is "logstash"
 if [ "$1" = 'logstash' ]; then
 	set -- gosu logstash "$@"
+	LS_JAVA_OPTS="-Xms$HEAP_SIZE -Xmx$HEAP_SIZE" $@ &
+else
+	$@ &
 fi
-$@ &
 
 fg
