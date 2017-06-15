a8451060170695363b7e955fbeb84b7ab9123c0a
khezen <guillaume@peaks.com>
2017-06-15 14:56:03 +0200

    #4 - fix TS_PWD in conf.d

--- src/miscellaneous/edit_config.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sed -ri "s/hosts =>[^\r\n]*/hosts => \"$ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT\"/" /etc/logstash/conf.d/logstash.conf
-sed -ri "s/password =>[^\r\n]*/password => \"$LOGSTASH_PWD\"/" /etc/logstash/conf.d/logstash.conf
